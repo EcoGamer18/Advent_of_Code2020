97d2c9674ed6f5c9ba3d4f8710138bb955f45c8e
EcoGamer18 <gabymeow07@gmail.com>
2020-12-08 08:44:14 +0200

    Day 8 done and day 7 botched

--- Day_7/day_7.py
+++ Day_7/day_7.py
@@ -1,8 +1,65 @@
+"""#botched version
+
+def find_index_bag(type_color, color, lista):
+    return [[i,j]
+            for i in range(len(lista))
+            for j in range(1,len(lista[i]))
+            if lista[i][j][1] == type_color and lista[i][j][2] == color
+            ]
+
+
+#bfs
+
+def resolve1(lista):
+    verificare = find_index_bag("shiny", "gold", lista)
+    done =[[lista[x[0]][0][0],lista[x[0]][0][1]] for x in verificare]
+    gold_len = len(verificare)
+    suma=0
+    for i in verificare:
+        type_color = lista[i[0]][0][0]
+        color= lista[i[0]][0][1]
+        suma+=lista[i[0]][i[1]][0]
+        verificare.extend([x
+                           for x in find_index_bag(type_color,color,lista)
+                           if [lista[x[0]][0][0],lista[x[0]][0][1]] not in done
+                           ])
+        done = [[lista[x[0]][0][0], lista[x[0]][0][1]] for x in verificare]
+    return suma- sum([lista[i[0]][i[1]][0] for i in verificare[:gold_len]])
+
+
 def read_file():
-    pass
+    with open("date_day_7.txt", "r") as fp:
+        lista = [[x for x in i.split(",")] for i in fp.read().split(".\n")]
+        for x in lista:
+            aux = []
+            x[0] = x[0].split(" contain ")
+            aux.append(x[0][1].split(" ")[:-1])
+            aux.extend(i.split(" ")[1:-1] for i in x[1:])
+            x[0] = x[0][0].split(" ")[:-1]
+            del x[1:]
+            for i in aux:
+                if i[0] == "no":
+                    i[0] = 0
+                    i[1] = ""
+                else:
+                    i[0] = int(i[0])
+            x.extend(aux)
+    return lista"""
+
+
+def read_file():
+    with open("date_day_7.txt","r") as fp:
+        lista = [x.split(" bags contain ")[0].split(", ")
+                for x in fp.read().split(".\n")
+            ]
+    return lista[:-1]
+
 
 def main_day_7():
-    lista=read_file()
+    lista = read_file()
+    for i in lista:
+        print(i)
+    #print(resolve1(lista))
 
 
 main_day_7()
---
+++ Day_8/day_8.py
@@ -0,0 +1,48 @@
+def resolve2(lista):
+    position = 0
+    while True:
+        if lista[position][0] == "nop":
+            a,b = resolve1([[lista[i][0],lista[i][1],0]  if i!=position else ["jmp",lista[i][1],0] for i in range(len(lista))])
+            if b==0:
+                return a
+        elif lista[position][0] == "jmp":
+            a, b = resolve1(
+                [[lista[i][0], lista[i][1], 0] if i != position else ["nop", lista[i][1], 0] for i in range(len(lista))])
+            if b == 0:
+                return a
+        position += 1
+    
+def resolve1(lista):
+    position=0
+    last_position=0
+    accumulator=0
+    while position<len(lista):
+        if lista[position][2]==1:
+            return accumulator,-1
+        last_position = position
+        if lista[position][0]=="nop":
+            pass
+        elif lista[position][0]=="acc":
+            accumulator+=lista[position][1]
+        elif lista[position][0]=="jmp":
+            position+=lista[position][1]-1
+        lista[position][2]+=1
+        position += 1
+    return accumulator,0
+
+def read_list():
+    with open("date_day_8.txt") as fp:
+        lista=[i.split(" ") for i in fp.read().splitlines()]
+        for i in lista:
+            i[1]=int(i[1])
+            i.append(0)
+    return lista
+
+def main_day_8():
+    lista=read_list()
+    """for i in lista:
+        print(i)"""
+    print(resolve1(lista))
+    print(resolve2(lista))
+
+main_day_8()
