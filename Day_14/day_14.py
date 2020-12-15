5ac587717b5e5c6749a5e85901f947bcc74fc565
EcoGamer18 <gabymeow07@gmail.com>
2020-12-15 19:46:13 +0200

    Day 14 done

--- Day_14/day_14.py
+++ Day_14/day_14.py
@@ -1,32 +1,67 @@
+import itertools
+from copy import deepcopy
+
+
+def masking_2(mask, value):
+    for i in range(36):
+        if mask[i] in ["1", "X"]:
+            value = value[:i] + mask[i] + value[i + 1:]
+    return value
+
+
+def floating_to_int(value):
+    values=[]
+    cartez = value.count("X")
+    if cartez == 0:
+        return bin_to_int(value)
+    else:
+        floating = list(itertools.product(["0", "1"], repeat=cartez))
+        for x in floating:
+            aux = deepcopy(value)
+            pos = 0
+            for i in range(36):
+                if aux[i] == "X":
+                    aux = aux[:i] + x[pos] + aux[i + 1:]
+                    pos += 1
+            aux = bin_to_int(aux)
+            values.append(aux)
+    return values
+
+
+def resolve2(lista):
+    final_sum = 0
+    memo = {}
+    for i in lista:
+        for j in i[1:]:
+            value_m = masking_2(i[0][0], dec_to_bin(int(j[0][4:-1])))
+            for x in floating_to_int(value_m):
+                memo[x] = int(j[1])
+    return sum(memo[i] for i in memo.keys())
+
+
+# -------------------------------------1----------------------------------------- #
+
 def masking_1(mask, value):
     for i in range(36):
-        if mask[i] in ["1","0"]:
-            value = value[:i] + mask[i] + value[i+1:]
+        if mask[i] in ["1", "0"]:
+            value = value[:i] + mask[i] + value[i + 1:]
     return value
 
 
 assert masking_1("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
-               "000000000000000000000000000000001011") \
+                 "000000000000000000000000000000001011") \
        == "000000000000000000000000000001001001"
 
-def masking_2(mask,value):
-    for i in range(36):
-        if mask[i]!='0':
-            value = value[:i] + mask[i] + value[i + 1:]
-    return value
-
-def resolve2(lista):
-    pass
 
 def resolve1(lista):
     final_sum = 0
-    memo = [0]*(max([int(y[0][4:-1]) for i in lista for y in i[1:]])+2)
+    memo = [0] * (max([int(y[0][4:-1]) for i in lista for y in i[1:]]) + 2)
     for i in lista:
         for j in i[1:]:
-            #print(i[0][0],dec_to_bin(int(j[1])),int(j[1]))
+            # print(i[0][0],dec_to_bin(int(j[1])),int(j[1]))
             value = masking_1(i[0][0], dec_to_bin(int(j[1])))
             memo[int(j[0][4:-1])] = bin_to_int(value)
-            #print(f" mem[{int(j[0][4:-1])}] = {value}")
+            # print(f" mem[{int(j[0][4:-1])}] = {value}")
     final_sum += sum(memo)
     return final_sum
 
@@ -63,7 +98,8 @@ def main_day_14():
     lista = read_file()
     """for i in lista:
         print(i)"""
-    print(f"First puzzle: {resolve1(lista)}")
+    # print(f"First puzzle: {resolve1(lista)}")
+    print(resolve2(lista))
 
 
 main_day_14()
